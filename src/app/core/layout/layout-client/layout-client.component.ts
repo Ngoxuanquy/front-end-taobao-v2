bab6156b83fb8f9b5836ba0f596d4db87fae0580
HaThiLinh2204 <111418592+HaThiLinh2204@users.noreply.github.com>
2025-04-10 00:17:50 +0700

    add footer

--- src/app/core/layout/layout-client/layout-client.component.ts
+++ src/app/core/layout/layout-client/layout-client.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../../components/header/header.component';
+import { FooterComponent } from '../../../components/footer/footer.component';
 
 @Component({
   selector: 'app-layout-client',
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [
+    RouterModule,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './layout-client.component.html',
   styleUrls: ['./layout-client.component.css']
 })
